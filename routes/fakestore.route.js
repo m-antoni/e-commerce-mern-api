6dece2f3ddeea14e6276b40687fe8c4efc4a432b
Michael Antoni <michaelantoni.cs@gmail.com>
2021-12-11 11:33:29 +0800

    update cors

--- routes/fakestore.route.js
+++ routes/fakestore.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const cors = require('cors');
 const FakeStoreController = require('../controllers/fakestore.controller');
 
 // URL: api/shipping
 
-router.get('/', FakeStoreController.getFakeStore);
+router.get('/', cors(), FakeStoreController.getFakeStore);
 
 module.exports = router;
